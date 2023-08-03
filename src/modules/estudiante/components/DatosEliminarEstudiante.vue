ec528ec0b95d88001fe014b37b0538e131f7c530
Your Name <you@example.com>
2023-08-03 18:13:18 -0500

    Prueba

--- src/modules/estudiante/components/DatosEliminarEstudiante.vue
+++ src/modules/estudiante/components/DatosEliminarEstudiante.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="datos">
     <input v-model="id" type="text" />
-    <button @click="eliminarEstudiante">Eliminar</button>
+    <button @click="eliminarEstudiante2">Eliminar</button>
   </div>
 </template>
 
 <script>
-import { eliminarEstudianteFachada } from "../helpers/EstudianteCliente.js";
+import { eliminarEstudianteFachada, eliminarPresentarEstudianteFachada } from "../helpers/EstudianteCliente.js";
 export default {
   data() {
     return {
@@ -16,6 +16,9 @@ export default {
 
   methods: {
     async eliminarEstudiante() {
+      await eliminarEstudianteFachada(this.id);
+    },
+     async eliminarEstudiante2() {
       await eliminarEstudianteFachada(this.id);
     },
   },
